e30676869e4889e9a53e567b0feded9cfba58986
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-24 12:12:11 +0300

    some remarks in documentation

--- widgets/ColoredWidget.py
+++ widgets/ColoredWidget.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QWidget, QHBoxLayout
 class ColoredWidget(QWidget):
     """additional widget, use it to set background color to widget"""
     def __init__(self, color):
-        """initialization"""
+        """initialization of container for widgets"""
         super(ColoredWidget, self).__init__()
         self.setAutoFillBackground(True)
 
