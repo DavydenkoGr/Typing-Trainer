722e90f41f7f764b739bcfe52a4aee30b6599693
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-04-24 12:09:36 +0300

    changed back clean -> clear

--- widgets/StatisticsWindow.py
+++ widgets/StatisticsWindow.py
@@ -27,7 +27,7 @@ class StatisticsWindow(QWidget):
         font.setPointSize(FONT_SIZE)
 
         self.set_statistics_list()
-        self.set_clean_button(font)
+        self.set_clear_button(font)
 
     def set_statistics_list(self):
         """sets list which displays all completed lessons statistics"""
@@ -36,16 +36,16 @@ class StatisticsWindow(QWidget):
 
         self.statistics.setStyleSheet("background-color: white")
 
-    def set_clean_button(self, font):
-        """sets button which cleans all completed lessons statistics"""
-        self.clean = QPushButton(self)
-        self.clean.setFixedWidth(int(STATISTICS_WIDTH * 0.8))
-        self.clean.clicked.connect(self.clean_all)
-        self.clean.setFocusPolicy(Qt.NoFocus)
+    def set_clear_button(self, font):
+        """sets button which clears all completed lessons statistics"""
+        self.clear = QPushButton(self)
+        self.clear.setFixedWidth(int(STATISTICS_WIDTH * 0.8))
+        self.clear.clicked.connect(self.clear_all)
+        self.clear.setFocusPolicy(Qt.NoFocus)
 
-        self.clean.setFont(font)
-        self.clean.setStyleSheet(f"background-color: {BUTTONS_COLOR}")
-        self.clean.setText("clean all")
+        self.clear.setFont(font)
+        self.clear.setStyleSheet(f"background-color: {BUTTONS_COLOR}")
+        self.clear.setText("clear all")
 
     def set_layout(self):
         """sets window layout"""
@@ -54,7 +54,7 @@ class StatisticsWindow(QWidget):
         layout.setContentsMargins(0, 0, 0, 0)
 
         layout.addWidget(self.statistics, alignment=Qt.AlignCenter)
-        layout.addWidget(self.clean, alignment=Qt.AlignCenter)
+        layout.addWidget(self.clear, alignment=Qt.AlignCenter)
 
         self.setLayout(layout)
 
@@ -65,24 +65,24 @@ class StatisticsWindow(QWidget):
 
     def show_statistics(self):
         """displays statistics to QPlainTextEdit"""
-        self.statistics.clean()
+        self.statistics.clear()
 
         try:
             file = open("resources/statistics.txt", "r")
             for line in file.readlines():
                 self.statistics.insertPlainText(line)
         except Exception:
-            self.statistics.clean()
+            self.statistics.clear()
             self.statistics.insertPlainText("unable to open file statistics.txt")
 
-    def clean_all(self):
-        """cleans all lessons statistics"""
+    def clear_all(self):
+        """clears all lessons statistics"""
         try:
             file = open("resources/statistics.txt", "w")
             file.close()
 
-            self.statistics.clean()
+            self.statistics.clear()
             self.statistics.insertPlainText("Success!")
         except Exception:
-            self.statistics.clean()
-            self.statistics.insertPlainText("unable to clean file statistics.txt")
+            self.statistics.clear()
+            self.statistics.insertPlainText("unable to clear file statistics.txt")
